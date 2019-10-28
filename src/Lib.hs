13628466eeed7cdfc1da536abc100f49ab8f36f3
hajlaoui-nader <hajlaoui.nader@gmail.com>
2019-10-28 15:31:17 +0100

    parser datatype

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
 module Lib
     ( someFunc
-    ) where
+    )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+myConcat :: [[a]] -> [a]
+myConcat []       = []
+myConcat (x : xs) = x ++ myConcat xs
+
+def :: String
+def = "hey"
+
+
+data Parser x = Parser(String -> Maybe (String, x))
+
+parseBool :: Parser Bool
+parseBool = Parser
+    (\s -> case s of
+        't' : z -> Just (z, True)
+        'f' : z -> Just (z, False)
+        _       -> Nothing
+    )
+
+fail :: Parser x
+fail = Parser (const Nothing)
+
+runParser :: Parser x -> String -> Maybe (String, x)
+runParser (Parser f) = f
+
+composeParsers :: Parser a -> Parser b -> Parser (a, b)
+composeParsers pa pb = Parser
+    (\s0 -> case runParser pa s0 of
+        Nothing      -> Nothing
+        Just (s1, a) -> case runParser pb s1 of
+            Nothing      -> Nothing
+            Just (s2, b) -> Just (s2, (a, b))
+    )
